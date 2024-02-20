489f4346fa88cb7853142e5f5f77ecda58605922
douglasrosa16 <douglasoliveira163425@gmail.com>
2024-02-20 13:21:49 -0400

    feat: ajuste script

--- projeto-01/script.js
+++ projeto-01/script.js
@@ -1,11 +1,17 @@
 function toggleMode(){
   const html = document.documentElement;
 
-  // if(html.classList.contains('light')){
-  //   html.classList.remove('light')
-  // }else{
-  //   html.classList.add('light')
-  // }
-
   html.classList.toggle('light'); //Procura a propriedade e troca ela, colocando ou removendo
+
+  //Pegar a tag img
+  const img = document.querySelector('#profile img');
+
+  if(html.classList.contains('light')){
+    img.setAttribute('src','./assets/avatar-light.png'); /*Aqui pode mudar a imagem */
+    img.setAttribute('alt','Imagem de Douglas sorrindo com Camisa Azul '); /*Aqui pode mudar a imagem */
+  }else{
+    img.setAttribute('src','./assets/avatar.png')
+  }
+
+  
 }
